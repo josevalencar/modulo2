e7b79b5d26e6294171d2513cdaf6ee2ac15f34ab
josevalencar <jose.silva@sou.inteli.edu.com.br>
2022-10-21 14:40:23 -0300

    finalizando ponderada - semana 2

--- 02_AUT_EST_ENTREGA/Semana 2/modeloFisico.sql
+++ 02_AUT_EST_ENTREGA/Semana 2/modeloFisico.sql
@@ -31,6 +31,8 @@ CREATE TABLE IF NOT EXISTS "habilidades" (
 	"nota"	INTEGER,
 	PRIMARY KEY("cod_habilidades")
 );
+INSERT INTO "usuario" ("cod_usuario","nome","cargo","endereco","tel","email","descricao") VALUES (1,'Jose Vitor','Programador','Rua Padre Vieira, 62 - Canindé','(11)97502-6556','jose.silva@sou.inteli.edu.br','isso e uma descricao');
 INSERT INTO "formacao" ("cod_formacao","nome_curso","data_inicio","data_fim","descricao_curso") VALUES (1,'eng computacao','2022-08-02','2026-06-02','um curso muito legal');
+INSERT INTO "experiencia" ("cod_experiencia","nome_empresa","nome_cargo","data_inicio","data_final","descricao_experiencia") VALUES (1,'inteli','estudante','2022-08-02','2026-06-02','descricao do curso');
 INSERT INTO "habilidades" ("cod_habilidades","ferramenta","nota") VALUES (1,'photoshop',4);
 COMMIT;
